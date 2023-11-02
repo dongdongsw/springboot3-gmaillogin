aebfe1dc3b0806f1693bdaee0c50b7ae54dd474f
This Develop <128954390+dongdongsw@users.noreply.github.com>
2023-11-02 16:56:06 +0900

    Add files via upload

---
+++ src/main/java/com.example.goglelogin/GogleloginApplication.java
@@ -0,0 +1,13 @@
+package com.example.goglelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogleloginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GogleloginApplication.class, args);
+    }
+
+}
